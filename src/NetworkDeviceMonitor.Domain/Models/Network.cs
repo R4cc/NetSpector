0b730fcf1ba034f833ce88afc3ddcb1c71224f2a
Niklas L <niklas.lerchenmueller@protonmail.com>
2022-07-29 11:35:58 +0200

    Fix validation Issue

--- src/NetworkDeviceMonitor.Domain/Models/Network.cs
+++ src/NetworkDeviceMonitor.Domain/Models/Network.cs
@@ -7,7 +7,7 @@ public class Network
     [Key]
     public int NetworkId { get; set; }
     public string IpNetworkId { get; set; }
-    public int SubnetMask { get; set; }
+    public int SubnetMask { get; set; } = 24;
     public string Name { get; set; }
     public List<Device> Devices { get; set; } = new();
 }
